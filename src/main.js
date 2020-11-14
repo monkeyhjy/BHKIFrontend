2427e1dbe0822618c0d297dc6e501ea30d35cf26
monkeyhjy <1246374291@qq.com>
2020-11-14 22:31:14 +0800

    初始化了前端少部分配置

--- src/main.js
+++ src/main.js
@@ -3,6 +3,14 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import Element from "element-ui";
+import axios from "axios";
+import qs from "qs";
+axios.defaults.baseURL='http://127.0.0.1:8000'
+axios.defaults.headers={'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8' }
+Vue.use(Element);
+Vue.prototype.$axios = axios;
+Vue.prototype.qs = qs;
 
 Vue.config.productionTip = false
 
