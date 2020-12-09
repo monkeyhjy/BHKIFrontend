bb97aec23076f5b8a09a6d1f63608af2a7cc7345
Manwei Zhang <shinyano@sina.com>
2020-12-10 00:33:47 +0800

    修正登录注册页面操作bug

--- src/views/user/Login.vue
+++ src/views/user/Login.vue
@@ -1,36 +1,36 @@
 <template>
   <div class="login">
-    <el-card class="login-card">
+    <el-card class="login-card" style="text-align:center">
       <img class="login-logo" src="../../assets/image/user/image/logoT.png">
       <el-tabs v-model="activeName" @tab-click="handleClick">
         <el-tab-pane label="登录" name="login">
-          <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm login-form">
+          <el-form :model="loginRuleForm" status-icon :rules="loginRules" ref="loginRuleForm" label-width="100px" class="demo-ruleForm login-form">
             <el-form-item label="邮箱" prop="email">
-              <el-input type="email" v-model="ruleForm.email" autocomplete="off"></el-input>
+              <el-input type="email" v-model="loginRuleForm.email" autocomplete="off"></el-input>
             </el-form-item>
             <el-form-item label="密码" prop="pass">
-              <el-input type="password" v-model="ruleForm.pass" autocomplete="off"></el-input>
+              <el-input type="password" v-model="loginRuleForm.pass" autocomplete="off"></el-input>
             </el-form-item>
             <el-form-item class="right-button">
-              <el-button type="primary" @click="submitForm('ruleForm')">登录</el-button>
-              <el-button @click="resetForm('ruleForm')">重置</el-button>
+              <el-button type="primary" @click="loginSubmitForm('loginRuleForm')">登录</el-button>
+              <el-button @click="resetForm('loginRuleForm')">重置</el-button>
             </el-form-item>
           </el-form>
         </el-tab-pane>
         <el-tab-pane label="注册" name="signup">
-          <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm login-form">
+          <el-form :model="signRuleForm" status-icon :rules="signRules" ref="signRuleForm" label-width="100px" class="demo-ruleForm login-form">
             <el-form-item label="用户名" prop="username">
-              <el-input type="text" v-model="ruleForm.username" autocomplete="off"></el-input>
+              <el-input type="text" v-model="signRuleForm.username" autocomplete="off"></el-input>
             </el-form-item>
             <el-form-item label="邮箱" prop="email">
-              <el-input type="email" v-model="ruleForm.email" autocomplete="off"></el-input>
+              <el-input type="email" v-model="signRuleForm.email" autocomplete="off"></el-input>
             </el-form-item>
             <el-form-item label="密码" prop="pass">
-              <el-input type="password" v-model="ruleForm.pass" autocomplete="off"></el-input>
+              <el-input type="password" v-model="signRuleForm.pass" autocomplete="off"></el-input>
             </el-form-item>
             <el-form-item class="right-button">
-              <el-button type="primary" @click="submitForm('ruleForm')">注册</el-button>
-              <el-button @click="resetForm('ruleForm')">重置</el-button>
+              <el-button type="primary" @click="signSubmitForm('signRuleForm')">注册</el-button>
+              <el-button @click="resetForm('signRuleForm')">重置</el-button>
             </el-form-item>
           </el-form>
         </el-tab-pane>
@@ -42,71 +42,112 @@
 <script>
 export default {
   data() {
-    var validateUsername = (rule, value, callback) => {
-        if (value === '') {
-          callback(new Error('请输入用户名'));
-        } else {
-          if (this.ruleForm.email !== '') {
-            this.$refs.ruleForm.validateField('email');
-          }
-          callback();
+    var validateSignUsername = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请输入用户名'));
+      } else {
+        if (this.signRuleForm.email !== '') {
+          this.$refs.signRuleForm.validateField('email');
         }
-      };
-      var validateEmail = (rule, value, callback) => {
-        if (value === '') {
-          callback(new Error('请输入邮箱'));
-        } else {
-          if (this.ruleForm.pass !== '') {
-            this.$refs.ruleForm.validateField('pass');
-          }
-          callback();
-        }
-      };
-      var validatePass = (rule, value, callback) => {
-        if (value === '') {
-          callback(new Error('请输入密码'));
-        } else {
-          callback();
-        }
-      };
-      return {
-        activeName: 'login',
-        ruleForm: {
-          email:'',
-          pass: '',
-          username: '',
-        },
-        rules: {
-          email: [
-            { validator: validateEmail, trigger: 'blur' }
-          ],
-          pass: [
-            { validator: validatePass, trigger: 'blur' }
-          ],
-          username: [
-            { validator: validateUsername, trigger: 'blur' }
-          ],
-        }
-      };
-    },
-    methods: {
-      handleClick(tab, event) {
-        console.log(tab, event);
-      },
-      submitForm(formName) {
-        this.$refs[formName].validate((valid) => {
-          if (valid) {
-            alert('submit!');
-          } else {
-            console.log('error submit!!');
-            return false;
-          }
-        });
-      },
-      resetForm(formName) {
-        this.$refs[formName].resetFields();
+        callback();
       }
+    };
+    var validateSignEmail = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请输入邮箱'));
+      } else {
+        if (this.signRuleForm.pass !== '') {
+          this.$refs.signRuleForm.validateField('pass');
+        }
+        callback();
+      }
+    };
+    var validateSignPass = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请输入密码'));
+      } else {
+        callback();
+      }
+    };
+    var validateLoginEmail = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请输入邮箱'));
+      } else {
+        if (this.loginRuleForm.pass !== '') {
+          this.$refs.loginRuleForm.validateField('pass');
+        }
+        callback();
+      }
+    };
+    var validateLoginPass = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请输入密码'));
+      } else {
+        callback();
+      }
+    };
+    return {
+      activeName: 'login',
+      signRuleForm: {
+        email:'',
+        pass: '',
+        username: '',
+      },
+      signRules: {
+        email: [
+          { validator: validateSignEmail, trigger: 'blur' }
+        ],
+        pass: [
+          { validator: validateSignPass, trigger: 'blur' }
+        ],
+        username: [
+          { validator: validateSignUsername, trigger: 'blur' }
+        ],
+      },
+      loginRuleForm: {
+        email:'',
+        pass: '',
+      },
+      loginRules: {
+        email: [
+          { validator: validateLoginEmail, trigger: 'blur' }
+        ],
+        pass: [
+          { validator: validateLoginPass, trigger: 'blur' }
+        ],
+      }
+    };
+  },
+  methods: {
+    handleClick(tab, event) {
+      console.log(tab, event);
+    },
+    loginSubmitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          this.$router.push("/search");
+        } else {
+          console.log('error submit!!');
+          return false;
+        }
+      });
+    },
+    signSubmitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          this.$alert('注册成功，请登录', '注册成功', {
+            confirmButtonText: '确定',
+          });
+        } else {
+          console.log('error submit!!');
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
     }
+  }
 }
 </script>
 
